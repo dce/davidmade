5b31d9b04182930043e7860a7f76706468636226
David Eisinger <david.eisinger@viget.com>
2011-03-09 23:50:40 -0500

    Indexes per post type

--- lib/layout.rb
+++ lib/layout.rb
@@ -1,8 +1,22 @@
 class Layout < Mustache
   self.template_file = "templates/layout.html.mustache"
 
-  def initialize(title = nil)
+  def initialize(opts = {})
     self[:yield] = yield
-    self[:title] = title || "Davidmade"
+    if opts[:post_type]
+      self[:post_type] = opts[:post_type]
+      self[:title] = "#{self[:post_type].capitalize} - Davidmade"
+    end
+    self[:title] ||= opts[:title] || "Davidmade"
+  end
+
+  def post_types
+    Post.types.map do |type|
+      {
+        :label => type.capitalize,
+        :url => "/#{type}",
+        :active => (type == self[:post_type])
+      }
+    end
   end
 end
--- lib/post.rb
+++ lib/post.rb
@@ -7,17 +7,24 @@ class Post < Mustache
     @meta, @body = File.open(@file_path).read.split(/\n\n/, 2)
     @meta = YAML.load(@meta)
     self.template = open("templates/_#{type}.html.mustache").read
-
-    open("public/#{url}", "w") do |index|
-      layout = Layout.new([title, "DavidMade"].compact * " - ") { render }
-      index.write(layout.render)
-    end
   end
 
   def self.all
     Dir.glob("posts/*").map {|f| new(f) }.sort.reverse
   end
 
+  def self.types
+    all.map {|p| p.type }.uniq
+  end
+
+  def write
+    open("public/#{url}", "w") do |index|
+      full_title = [title, "DavidMade"].compact * " - "
+      layout = Layout.new(:title => full_title) { render }
+      index.write(layout.render)
+    end
+  end
+  
   def date_time
     DateTime.parse(@meta["date"].to_s)
   end
