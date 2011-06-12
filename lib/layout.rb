1d92bd359d0da34a7f15ffb9056b363c0098ba0d
David Eisinger <david.eisinger@viget.com>
2011-06-12 13:46:24 -0400

    Layout code formatting

--- lib/layout.rb
+++ lib/layout.rb
@@ -3,20 +3,20 @@ class Layout < Mustache
 
   def initialize(opts = {})
     self[:yield] = yield
+
     if opts[:post_type]
       self[:post_type] = opts[:post_type]
       self[:title] = "#{self[:post_type].capitalize} - Davidmade"
     end
+
     self[:title] ||= opts[:title] || "Davidmade"
   end
 
   def post_types
     Post.types.map do |type|
-      {
-        :label => type.capitalize,
+      { :label => type.capitalize,
         :url => "/#{type}/",
-        :active => (type == self[:post_type])
-      }
+        :active => (type == self[:post_type]) }
     end
   end
 end
